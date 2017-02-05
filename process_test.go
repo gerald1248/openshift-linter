4517094f647833718a8c436aa77e8bc37e9e13ab
gerald1248 <gerald1248@gmail.com>
2017-02-05 09:52:32 +0100

    added tests

---
+++ process_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestProcessFile(t *testing.T) {
+	invalidPath := "/non/existent/file.yaml"
+	params := LinterParams{"", "", "", "", "", "json"}
+	_, err := processFile(invalidPath, params)
+
+	if err == nil {
+		t.Errorf("Must reject invalid path %s", invalidPath)
+	}
+}
+
+func TestProcessBytes(t *testing.T) {
+	//don't allow XML
+	xmlBuffer := []byte(`<?xml version="1.0" encoding="UTF-8" standalone="true"?><root/>`)
+	params := LinterParams{"", "", "", "", "", "xml"}
+	_, err := processBytes(xmlBuffer, params)
+
+	if err == nil {
+		t.Errorf("Must reject XML input")
+	}
+}
